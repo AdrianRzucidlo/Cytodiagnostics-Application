5c000a0e403a0086f2b560bf3f1c58bb945653c9
AdrianRzucidlo <adrianrzucidlo2001@gmail.com>
2022-04-27 20:34:11 +0200

    Dodaj pliki projektów.

---
+++ Cytodiagnostics-Application/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cytodiagnostics_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
